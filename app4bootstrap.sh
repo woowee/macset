fd4a958bf8319bfda0644a5f33544af3fdeaaaea
woowee <wooweezoowee@gmail.com>
2016-09-24 18:50:52 +0900

    app4bootstrap.sh: remove the process of item2.;

--- app4bootstrap.sh
+++ app4bootstrap.sh
@@ -117,51 +117,6 @@ else
 fi
 
 
-# iTerm2.app
-execho "${app_iterm2_filename} Settings..."
-if check_existence_app "${app_iterm2_filename}" path_app; then
-    execho "${app_iterm2_filename}= ${path_app}"
-
-    #execho "opening iterm2. please wait ..."
-    #open -W -a "${app_iterm2_filename}"
-    cd "${dir_tmp}"
-
-    curl -o com.googlecode.iterm2.plist https://gist.githubusercontent.com/woowee/53fd864693f2ea01a247/raw/1efbfd5578149fac9937107f80859d5058bc4e07/com.googlecode.iterm2.plist
-    cp -f com.googlecode.iterm2.plist ${HOME}/Library/Preferences
-
-    curl -o "Solarized Dark.itermcolors" https://raw.githubusercontent.com/altercation/solarized/master/iterm2-colors-solarized/Solarized%20Dark.itermcolors
-    open "Solarized Dark.itermcolors"; rm "Solarized Dark.itermcolors"
-
-    curl -o Hybrid.itermcolors https://gist.githubusercontent.com/w0ng/5e0a431531670e05dc4f/raw/138b83d2736070f7b089a1ff22068c2d1702cf6c/gistfile1.txt
-    open Hybrid.itermcolors; rm Hybrid.itermcolors
-    # ref.https://github.com/databus23/dotfiles/blob/master/osx
-
-    plist="${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
-    if [ -e "${plist}" ]; then
-        #blur
-        sudo /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Blur\" true" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-        sudo /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Blur Radius\" 2.500" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-        #transparency
-        sudo /usr/libexec/PlistBuddy -c "Print :\"New Bookmarks\":0:\"Transparency\"" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-        sudo /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Transparency\" 0.250" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-        #window type
-        sudo /usr/libexec/PlistBuddy -c "Print :\"New Bookmarks\":0:\"Window Type\"" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-        sudo /usr/libexec/PlistBuddy -c "Set :\"New Bookmarks\":0:\"Window Type\" 2" ${HOME}/Library/Preferences/com.googlecode.iterm2.plist
-
-        # Don’t display the annoying prompt when quitting iTerm
-        defaults write com.googlecode.iterm2 PromptOnQuit -bool false
-    else
-        execho "Oops! ${plist} was not found. Please set preferences of iTerm2 later."
-    fi
-
-    cd ${HOME}
-
-else
-    execho "Sorry, ${app_iterm2_filename} was not found..."
-fi
-
-# Terminal.app
-# does it mean that can not set itself while using Terminal.app itself ?
 
 # MacVim.app + plugins, and settings for personal runtimepath (ex. ftplugins)
 execho "${app_macvim_filename} Settings..."
