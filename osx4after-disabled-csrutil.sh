f7ffe4e314341ad50ff9e2830f500ab4d884ea05
woowee <wooweezoowee@gmail.com>
2017-07-27 00:31:03 +0900

    osx4after-disabled-csrutil.sh:Changed process for JapaneseIM.;

--- osx4after-disabled-csrutil.sh
+++ osx4after-disabled-csrutil.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -u
 
-set -e
 #
 # PREPARE
 #
@@ -63,54 +62,82 @@ function ask {
 }
 
 if ask 'Input: 数字，記号はシングルバイトでの入力にする．' Y; then
-    pb=/usr/libexec/PlistBuddy
-    plistis=/System/Library/Input\ Methods/JapaneseIM.app/Contents/Resources/KeySetting_Default.plist
+  #
+  # Store `PlistBuddy` path
+  #
+  # Expected Result:
+  #     pb=/usr/libexec/PlistBuddy
 
-    function keyCharExistence {
+  pb=$(mdfind -onlyin /usr/libexec -name Plistbuddy)
 
-      val=$("${pb}" -c "Print :keys:${1}:${2}:character" "${plistis}" 2>/dev/null || true)
 
-      if [ "${val}" = "" ]; then
-        # echo "add"
-        "${pb}" -c "Add :keys:${1}:${2}:character string ${3}" "${plistis}"
-      else
-        # echo "set(modify)"
-        "${pb}" -c "Set :keys:${1}:${2}:character ${3}" "${plistis}"
-      fi
-    }
+  #
+  # Store `KeySetting_Default.plist` path
+  #
+  # Expected Result:
+  #     plistis=/System/Library/Input Methods/JapaneseIM.app/Contents/PlugIns/JapaneseIM.appex/Contents/Resources/KeySetting_Default.plist
 
-    sudo cp -f "${plistis}" "$HOME/KeySetting_Default.plist.org"
+  echo "'locate' コマンドの準備 1/2; データベースの生成..."
+  sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.locate.plist &
+  wait
+  echo "...終了．"
 
-    sudo "${pb}" -c "Set :keys:before_typing:\'' \'':character ' '" "${plistis}" # 　   space
-    #sudo "${pb}" -c "Set :keys:*:\''-\'':character '-'" "${plistis}"            # －   minus
-    #sudo "${pb}" -c "Set :keys:*:\''[\'':character '['" "${plistis}"            # 「」 bracket
-    #sudo "${pb}" -c "Set :keys:*:\'']\'':character ']'" "${plistis}"
-    sudo "${pb}" -c "Set :keys:*:\''|\'':character '|'" "${plistis}"             # ｜   vertical bar
-    sudo "${pb}" -c "Set :keys:*:\''\!\'':character '\!'" "${plistis}"           # ！   exclamation
-    sudo "${pb}" -c "Set :keys:*:\''"'\"'"\'':character '"'\"'"'" "${plistis}"   # ”   double quotation
-    sudo "${pb}" -c "Set :keys:*:\''#\'':character '#'" "${plistis}"             # ＃   sharp
-    sudo "${pb}" -c "Set :keys:*:\''$\'':character '$'" "${plistis}"             # ＄   dollar
-    sudo "${pb}" -c "Set :keys:*:\''%\'':character '%'" "${plistis}"             # ％   percent
-    sudo "${pb}" -c "Set :keys:*:\''&\'':character '&'" "${plistis}"             # ＆   ampersand
-    sudo "${pb}" -c "Set :keys:*:\''\'\'':character '\''" "${plistis}"           # ’   apostrophe(single quotation)
-    sudo "${pb}" -c "Set :keys:*:\''(\'':character '('" "${plistis}"             # （） parentheses
-    sudo "${pb}" -c "Set :keys:*:\'')\'':character ')'" "${plistis}"
-    sudo "${pb}" -c "Set :keys:*:\''*\'':character '*'" "${plistis}"             # ＊   asterisk
-    sudo "${pb}" -c "Set :keys:*:\''+\'':character '+'" "${plistis}"             # ＋   plus
-    sudo "${pb}" -c "Set :keys:*:\''\:\'':character ':'" "${plistis}"            # ：   colon
-    sudo "${pb}" -c "Set :keys:*:\'';\'':character ';'" "${plistis}"             # ；   semicolon
-    sudo "${pb}" -c "Set :keys:*:\''<\'':character '<'" "${plistis}"             # ＜＞ angle bracket
-    sudo "${pb}" -c "Set :keys:*:\''>\'':character '>'" "${plistis}"
-    sudo "${pb}" -c "Set :keys:*:\''=\'':character '='" "${plistis}"             # ＝   equals
-    sudo "${pb}" -c "Set :keys:*:\''?\'':character '?'" "${plistis}"             # ？   question
-    sudo "${pb}" -c "Set :keys:*:\''@\'':character '@'" "${plistis}"             # ＠   at
-    sudo "${pb}" -c "Set :keys:*:\''^\'':character '^'" "${plistis}"             # ＾   caret
-    sudo "${pb}" -c "Set :keys:*:\''_\'':character '_'" "${plistis}"             # ＿   underscore
-    sudo "${pb}" -c "Set :keys:*:\''\`\'':character '\`'" "${plistis}"           # ‘   back quote
-    keyCharExistence "before_typing" "\''/\''" "'/'"                             # ／ slash (solidus)
-    keyCharExistence "typing" "\''/\''" "'/'"
-    keyCharExistence "before_typing" "\''\\\\\''" "'\\\'"                        # ＼ backslash (reverse solidus)
-    keyCharExistence "typing" "\''\\\\\''" "'/'"
+  echo "'locate' コマンドの準備 2/2; データベースを更新..."
+  sudo /usr/libexec/locate.updatedb &
+  wait
+  echo "...終了．"
+
+  plistis=$(sudo locate  "KeySetting_Default.plist")
+  if [ -z "${plistis}" ]; then
+    echo "'KeySetting_Default.plist' が見つかりませんでした。処理を中断しました。"
+    exit 1
+  fi
+
+  function keyCheck {
+
+    echo "${pb}" -c "Print :keys:${1}:${2}:character" "${plistis}"
+    val=$("${pb}" -c "Print :keys:${1}:${2}:character" "${plistis}" 2>/dev/null || true)
+
+    if [ "${val}" = "" ]; then
+      echo "add ${2}"
+      sudo "${pb}" -c "Add :keys:${1}:${2}:character string ${3}" "${plistis}"
+    else
+      echo "set(modify) ${2}"
+      sudo "${pb}" -c "Set :keys:${1}:${2}:character ${3}" "${plistis}"
+    fi
+  }
+
+  # back up the original plist file
+  [ ! -e ${HOME}/temp ] && mkdir ${HOME}/temp
+  sudo cp -f "${plistis}" "${HOME}/temp/KeySetting_Default.plist~$(date '+%Y%m%d%H%M')"
+
+  sudo "${pb}" -c "Set :keys:before_typing:\'' \'':character ' '" "${plistis}"  # 　   space
+  sudo "${pb}" -c "Set :keys:*:\''|\'':character '|'" "${plistis}"              # ｜   vertical bar
+  sudo "${pb}" -c "Set :keys:*:\''\!\'':character '\!'" "${plistis}"            # ！   exclamation
+  sudo "${pb}" -c "Set :keys:*:\''"'\"'"\'':character '"'\"'"'" "${plistis}"    # ”   double quotation
+  sudo "${pb}" -c "Set :keys:*:\''#\'':character '#'" "${plistis}"              # ＃   sharp
+  sudo "${pb}" -c "Set :keys:*:\''$\'':character '$'" "${plistis}"              # ＄   dollar
+  sudo "${pb}" -c "Set :keys:*:\''%\'':character '%'" "${plistis}"              # ％   percent
+  sudo "${pb}" -c "Set :keys:*:\''&\'':character '&'" "${plistis}"              # ＆   ampersand
+  sudo "${pb}" -c "Set :keys:*:\''\'\'':character '\''" "${plistis}"            # ’   apostrophe(single quotation)
+  sudo "${pb}" -c "Set :keys:*:\''(\'':character '('" "${plistis}"              # （） parentheses
+  sudo "${pb}" -c "Set :keys:*:\'')\'':character ')'" "${plistis}"
+  sudo "${pb}" -c "Set :keys:*:\''*\'':character '*'" "${plistis}"              # ＊   asterisk
+  sudo "${pb}" -c "Set :keys:*:\''+\'':character '+'" "${plistis}"              # ＋   plus
+  sudo "${pb}" -c "Set :keys:*:\''\:\'':character ':'" "${plistis}"             # ：   colon
+  sudo "${pb}" -c "Set :keys:*:\'';\'':character ';'" "${plistis}"              # ；   semicolon
+  sudo "${pb}" -c "Set :keys:*:\''<\'':character '<'" "${plistis}"              # ＜＞ angle bracket
+  sudo "${pb}" -c "Set :keys:*:\''>\'':character '>'" "${plistis}"
+  sudo "${pb}" -c "Set :keys:*:\''=\'':character '='" "${plistis}"              # ＝   equals
+  sudo "${pb}" -c "Set :keys:*:\''?\'':character '?'" "${plistis}"              # ？   question
+  sudo "${pb}" -c "Set :keys:*:\''@\'':character '@'" "${plistis}"              # ＠   at
+  sudo "${pb}" -c "Set :keys:*:\''^\'':character '^'" "${plistis}"              # ＾   caret
+  sudo "${pb}" -c "Set :keys:*:\''_\'':character '_'" "${plistis}"              # ＿   underscore
+  sudo "${pb}" -c "Set :keys:*:\''\`\'':character '\`'" "${plistis}"            # ‘   back quote
+  keyCheck "before_typing" "\''/\''" "'/'"                                      # ／ slash (solidus)
+  keyCheck "typing" "\''/\''" "'/'"
+  keyCheck "before_typing" "\''\\\\\''" "'\\\'"                                 # ＼ backslash (reverse solidus)
+  keyCheck "typing" "\''\\\\\''" "'\\\'"
 fi
 
 if ask 'Finder: フォルダの名称は、英語表記．' Y; then
